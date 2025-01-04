4cdafd1a2f68438ffc904a80e9a5f040de242f31
Cenk Aktas <cnktass@gmail.com>
2025-01-04 15:10:10 +0300

    Tum Arayuzlerinin Yapilmasi

--- MvcCv/Controllers/DefaultController.cs
+++ MvcCv/Controllers/DefaultController.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using MvcCv.Data;
+using MvcCv.Models.Entity;
 
 namespace MvcCv.Controllers
 {
@@ -20,5 +21,54 @@ namespace MvcCv.Controllers
             var deneyimler = _context.Deneyimlerim.ToList();
             return PartialView("Deneyim", deneyimler);
         }
+        public PartialViewResult Egitimlerim()
+        {
+            var egitimler = _context.Egitimlerim.ToList();
+            return PartialView("Egitimlerim", egitimler);
+        }
+        public PartialViewResult Yeteneklerim()
+        {
+            var yetenekler = _context.Yeteneklerim.ToList();
+            return PartialView("Yeteneklerim", yetenekler);
+        }
+        public PartialViewResult Hobilerim()
+        {
+            var hobiler = _context.Hobilerim.ToList();
+            return PartialView("Hobilerim", hobiler);
+        }
+        public PartialViewResult Sertifikalarim()
+        {
+            var sertifikalar = _context.Hobilerim.ToList();
+            return PartialView("Sertifikalarim", sertifikalar);
+        }
+
+        [HttpGet]
+        public PartialViewResult Iletisim()
+        {
+            var model = new TblIletisim(); 
+            return PartialView("Iletisim", model);
+        }
+
+        [HttpPost]
+        public IActionResult Iletisim(TblIletisim iletisim)
+        {
+            if (ModelState.IsValid)
+            {
+                iletisim.Tarih = DateTime.Parse(DateTime.Now.ToShortDateString());
+                _context.Iletisim.Add(iletisim);
+                _context.SaveChanges();
+                TempData["Message"] = "Mesajınız başarıyla gönderildi!";
+            }
+            else
+            {
+                TempData["Message"] = "Lütfen tüm alanları doğru doldurun.";
+            }
+
+            return RedirectToAction("Index", "Default");
+        }
+
+
+
+
     }
 }
